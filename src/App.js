eceefe478e00781a1ec53fdf70d881fe00c97d26
Amelia Schulz <amelia.schulz@gmail.com>
2018-12-06 17:20:44 -0500

    creates parent coffeerun component:

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import Coffee from './Coffee';
+import CoffeeRun from './CoffeeRun';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Coffee />
+        <CoffeeRun />
         
       </div>
     );
