389fc9fc01b08519c855774b5a9818f75b4f0d52
Amelia Schulz <amelia.schulz@gmail.com>
2018-12-06 17:11:34 -0500

    renders coffee form

--- src/Coffee.js
+++ src/Coffee.js
@@ -17,13 +17,48 @@ class Coffee extends Component {
                 <h1>CoffeeRun</h1>
                 <p>How would you like your caffeine?</p>
                 <form>
-                <input 
-                
-                onChange={this._onChange}
-                placeholder="enter order"
-                />
+                    <label>Coffee:
+                    <input 
+                    onChange={this._onChange}
+                    placeholder="caramel macchiato"
 
+                    />
+                    </label>
+                    <br></br>
+                    <label>Email:
+                    <input 
+                    onChange={this._onChange}
+                    type='email'
+                    placeholder="me@me.com"
                     
+                    />
+                    </label>
+                    <br></br>
+                    <label>Flavor:
+                    <input 
+                    onChange={this._onChange}
+                    placeholder="black"
+                    
+                    />
+                    </label>
+                    <br></br>
+                    <label>Strength:
+                    <input 
+                    onChange={this._onChange}
+                    placeholder="santas elves"
+                    
+                    />
+                    </label>
+                    <br></br>
+                    <label>Size:
+                    <input 
+                    onChange={this._onChange}
+                    placeholder="human"
+                    
+                    />
+                    </label>
+                    <br></br>
+                    <input type="submit" />
                 </form>
             </div>
         )
